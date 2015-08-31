f1bbcc15703c8ccd93552a15873ffedd6af7022c
Duy Tang <dtang3@umbc.edu>
2015-08-31 11:10:52 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/duy/timerbeta/MainActivity.java
+++ app/src/main/java/com/example/duy/timerbeta/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends Activity {
         w10.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-                work.setText("1");
+                work.setText("10");
 
             }
         });
